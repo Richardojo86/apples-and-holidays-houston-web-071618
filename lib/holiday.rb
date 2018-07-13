dcd40a5af29a2921580ea3f3e9e26a214c940fc2
Richard Ojo <richardojo04@gmail.com>
2018-07-13 15:46:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do | parent_key, parent_value|
      puts "#{parent_key.capitalize}:"
-     parent_value.each do |holiday, item|
-       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
+     parent_value.each do |child_key, item|
+       puts "  #{child_key.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
   end 
 end
